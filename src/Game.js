a422535674766335df3bd8a90a6e60458a8aa637
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-06-26 12:31:36 +0900

    Create Game.js

---
+++ src/Game.js
@@ -0,0 +1,88 @@
+const { convtext, fetch } = require("./system/");
+
+class Game {
+  constructor(client) {
+    this.ranking = new Ranking(client);
+  }
+}
+
+class Ranking {
+  constructor(client) {
+    this.client = client
+  }
+  async get(ranking, cache = true) {
+    let result = new Map(),
+      temp = new Array();
+    let source = await fetch("http://himaquest.com/mission_Ranking.php");
+    source = source.source
+      .split("<BR>")
+      .join("<br>")
+      .split("\t")
+      .join("")
+      .split("\r\n")
+      .join("")
+      .split("<tr><td>");
+    source.shift();
+    for (let i = 0; i < source.length; i++) {
+      temp = source[i].split("</td><td>");
+      const obj = new Object();
+      const id = Number(
+        temp[2].split("<small style='color:#AAAAAA'>")[1].split("</small>")[0]
+      );
+      obj.user = this.client?.secret?.id
+        ? cache
+          ? this.client.secret.options.includes(1 << 2)
+            ? this.client.users.fetch(id)
+            : this.client.users.get(id)
+          : await this.client.fetch(id)
+        : {
+            name: convtext(temp[2].split("<small style='color:#AAAAAA'>")[0]),
+            id: id,
+          };
+      obj.mission = temp[3];
+      obj.date = temp[4].split("</td></tr>")[0];
+      result.set(temp[0], obj);
+    }
+    if (ranking) {
+      return result.get(ranking.toString());
+    }
+    return result;
+  }
+}
+
+class UpdateInfo {
+  async get(date = null) {
+    let results = "",
+      result = new Map(),
+      temp = new Array();
+    await fetch("http://himaquest.com/top_UpdateInfo.php").source;
+    let source = results
+      .split("<BR>")
+      .join("<br>")
+      .slice(33)
+      .slice(0, -12)
+      .split("<br>\r\n");
+    for (let i = results.length - 1; i >= 0; i--) {
+      if (
+        /^[0-9]{4}\/([1-9]|1[0-2])\/([1-9]|[12][0-9]|3[01])/.test(
+          results[i].trim()
+        )
+      ) {
+        result.set(results[i], temp);
+        temp = new Array();
+      } else {
+        temp.unshift(results[i]);
+      }
+      if (date) {
+        if (typeof date == "string") {
+          return result.get(date);
+        } else {
+          return result.get(
+            `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+          );
+        }
+      }
+      return result;
+    }
+  }
+}
