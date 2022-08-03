1f4224eba438880bd2b8090f92825552b19ae43f
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-08-03 17:14:34 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,99 +1,94 @@
 "use strict";
 const { EventEmitter } = require("node:events");
-const { fetch } = require("./system/");
+const { fetch, OptionBits } = require("./system/");
 
-class Base extends EventEmitter {
-}
+class Base extends EventEmitter {}
 
 class Data {
-  constructor(a,b){
-    for (const n in a){
-      this[n] = a[n]
-    }
-    this.client = b
-    this.parseData = a
-  }
-  toString(){
-    return this.name
-  }
-  clone(){
-    return new this.constructor(this.parseData)
-  }
+	static parseData;
+	constructor(a, b) {
+		for (const n in a) {
+			this[n] = a[n];
+		}
+		this.client = b;
+		this.parseData = a;
+	}
+	toString() {
+		return this.name;
+	}
+	clone() {
+		return new this.constructor(this.parseData);
+	}
 }
 
 class Client extends Base {
-  constructor(options) {
-    super();
-    this.id = "";
-    this.pass = "";
-    this.users = require("./user/");
-    this.guilds = require("./guild/");
-    this.secret = {
-      id: undefined,
-      key: undefined,
-      options: options
-        ? typeof options == "Object"
-          ? OptionsToArray(options)
-          : options
-        : new Array(),
-    };
-  }
-  async login(id = this.id, pass = this.pass) {
-    if (!id || !pass)
-      return new Error("ID or PASS is invalid.(Error Code 100)");
-    if (!id.match(/^[a-z0-9]{4,20}$/) || !pass.match(/^[a-z0-9]{4,20}$/))
-      return new Error("ID or PASS is invalid.(Error Code 101)");
-    const result = await fetch("http://himaquest.com/top_LoginGame2.php", {
-      fid: id,
-      fpass: pass,
-      hkey: 1,
-    });
-    if (result.error == 2) {
-      return new Error("ID or PASS is wrong. (Error Code 102)");
-    } else if (result.error == 404) {
-      return new Error("ERROR 404 (Banned.)");
-    } else {
-      this.secret.id = result.userid
-      this.secret.key = result.seskey
-      this.guilds = new this.guilds(this);
-      this.users = new this.users(this);
-      this.user = await this.users.fetch(result.userid);
-      this.guild = this.user.guild
-      this.emit("ready", this);
+	constructor(options) {
+		super();
+		this.id = "";
+		this.pass = "";
+		this.users = require("./user/");
+		this.guilds = require("./guild/");
+		if (options.option == undefined) throw new Error("Option is invalid.")
+		this.secret = {
+			id: undefined,
+			key: undefined,
+			options: OptionBits.set(options.option),
+			logined: false
+		};
+	}
+	async login(id = this.id, pass = this.pass) {
+		if (this.secret.logined) throw new Error("Already Logined.")
+		this.emit("debug", "[Debug] Login Requested.")
+		this.emit("debug", `[Debug] Recieved ID:${id.slice(0,2)}${id.slice(2).replace(/./g, "*")} Recieved PASS:${pass.slice(0,2)}${pass.slice(2).replace(/./g, "*")}`)
+		if (!id || !pass) throw new Error("ID or PASS is invalid.(Error Code 100)");
+		if (!id.match(/^[a-z0-9]{4,20}$/) || !pass.match(/^[a-z0-9]{4,20}$/))
+			return new Error("ID or PASS is invalid.(Error Code 101)");
+		const result = await fetch("http://himaquest.com/top_LoginGame2.php", {
+			fid: id,
+			fpass: pass,
+			hkey: 1,
+		});
+		if (result.error == 2) {
+			throw new Error("ID or PASS is wrong. (Error Code 102)");
+		} else if (result.error == 404) {
+			throw new Error("ERROR 404 (Banned.)");
+		} else {
+			this.secret.logined = true
+			this.secret.id = result.userid;
+			this.secret.key = result.seskey;
+			this.guilds = new this.guilds(this);
+			this.users = new this.users(this);
+			this.user = await this.users.fetch(result.userid);
+			this.guild = this.user.guild;
+			this.emit("ready", this);
+			this.logined = true
+			const { startload } = require("./chat/");
+			startload(this, result.kbmark);
 
-      const { startload } = require("./chat/");
-      startload(this, result.kbmark);
-
-      return true;
-    }
-  }
+			return true;
+		}
+	}
+	async logout(send = true){
+		if (!this.secret.logined) throw new Error("Already Logouted.")
+		this.secret.logined = false
+		this.emit("debug", "[Debug] Logout Requested.")
+		if (send) await fetch("http://himaquest.com/top_ExitGame.php", {
+			marumie: this.secret.id,
+			seskey: this.secret.key
+		})
+		this.secret = {
+			id: "",
+			key: "",
+			users: require("./user/"),
+			guilds: require("./guild/")
+		}
+		this.emit("debug", "[Debug] Logouted.")
+	}
 }
 
-function OptionsToArray(options) {
-  const array = new Array();
-  if (options.SaveCache === false) {
-    if (array.NotSaveCache === false) return new Error("Options is invalid.(Code:001)");
-    if (!array.includes("NotSaveCache")) array[array.length] = 1 << 1
-  }
-  if (options.NotSaveCache === true) {
-    if (array.SaveCache === true) return new Error("Options is invalid.(Code:002)");
-    if (!array.includes("NotSaveCache")) array[array.length] = 1 << 1
-  }
-  if (options.LoadCache === false) {
-    if (array.NotLoadCache === false) return new Error("Options is invalid.(Code:001)");
-    if (!array.includes("NotLoadCache")) array[array.length] = 1 << 2
-  }
-  if (options.NotLoadCache === true) {
-    if (array.LoadCache === true) return new Error("Options is invalid.(Code:002)");
-    if (!array.includes("NotLoadCache")) array[array.length] = 1 << 2
-  }
-  if (options.RecieveAreaMessage === true){
-    array[array.length] = 1 << 3
-  }
-}
-
-exports.Client = Client
-exports.Base = Base
-exports.Data = Data
-exports.Errors = require("./Errors.js")
-exports.Game = require("./Game.js")
+exports.Client = Client;
+exports.Base = Base;
+exports.Data = Data;
+exports.OptionBits = OptionBits;
+exports.Errors = require("./Errors.js");
+exports.Game = require("./Game.js");
