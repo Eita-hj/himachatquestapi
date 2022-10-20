9ad5c9f4e3febdc88bd10b8112334d2cf4c8aa0e
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-20 13:35:03 +0900

    FileAttachmentの変更に伴う更新

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-module.exports.GuildMessageAttchment = require("./structures/GuildMessageAttachent");
-module.exports.DMMessageAttchment = require("./structures/DMMessageAttachent");
+module.exports.GuildMessageAttachment = require("./structures/GuildMessageAttachment");
+module.exports.DMMessageAttachment = require("./structures/DMMessageAttachment");
 module.exports.Client = require("./client/");
 module.exports.OptionBits = require("./structures/OptionBits");
 module.exports.Errors = require("./structures/Errors");
