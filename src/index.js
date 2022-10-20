24d4762c42e65317df86d0f70cf2d9461be2734d
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-20 10:48:48 +0900

    FileAttchmentの分離に伴う更新

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-module.exports = require("./structures/FileAttachent");
+module.exports.GuildMessageAttchment = require("./structures/GuildMessageAttachent");
+module.exports.DMMessageAttchment = require("./structures/DMMessageAttachent");
 module.exports.Client = require("./client/");
 module.exports.OptionBits = require("./structures/OptionBits");
 module.exports.Errors = require("./structures/Errors");
