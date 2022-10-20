f6e511518a8a29919ea389f397ff5fb3c19307cb
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-20 11:40:13 +0900

    ChatCollectorから分離

---
+++ src/collectors/AreaChatCollector.js
@@ -0,0 +1,46 @@
+const { api, convtext } = require("../utils/");
+
+module.exports = async function (client, defaultbmark) {
+  let first = true
+	let bmark = await api.post(api.links.Chat.AreaMessage, {
+		marumie: client.secret.id,
+		seskey: client.secret.key,
+		bmark: defaultbmark,
+	}).bmark;
+	for (; client.secret.load; ) {
+		if (!client.secret.load) return;
+		const data = await api.post(api.links.Chat.AreaMessage, {
+			marumie: client.secret.id,
+			seskey: client.secret.key,
+			bmark,
+		});
+		if (data.coments.length) {
+			if (!first){
+				for (let i = 0; i < data.coments.length; i++) {
+					bmark = data.bmark == undefined ? bmark : data.bmark;
+					let source = data.coments[i].source;
+					let result = new Object();
+					result.content = source
+						.split("\t")
+						.join("")
+						.split("<td class='c_mozi' style='color:#000000'>")[1]
+						.split("\n")[0]
+					result.shout = result.content.includes("<b style=")
+					result.content = convtext(result.content)
+					result.content = result.shout ? result.content.slice(25).slice(0, -4) : result.content
+					if (!client.secret.load) return;
+					result.author = client.secret.options.has(1n << 2n)
+						? await client.users.fetch(data.coments[i].uid)
+						: await client.users.get(data.coments[i].uid);
+					if (!client.secret.load) return;
+					client.emit("AreaMessageCreate", result);
+				}
+			} else {
+				bmark = data.bmark == undefined ? bmark : data.bmark;
+				first = false;
+			}
+		}
+		if (!client.secret.load) return;
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+	}
+}
