336d83bf663eaf9a4c8905e37e59d6da8a92a8ba
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-20 11:36:39 +0900

    ChatCollectorから分離

---
+++ src/collectors/GuildChatCollector.js
@@ -0,0 +1,74 @@
+const { api, convtext } = require("../utils/");
+
+module.export = async function getGuildChat(client) {
+	let bmark = await api.post(api.links.Guild.ChatEntry, {
+		marumie: client.secret.id,
+		seskey: client.secret.key,
+	});
+	bmark = bmark.bmark
+	for (let rate = 1; true; rate++) {
+		if (!client.secret.chatload) return;
+		const obj = await api.post(api.links.Chat.GuildMessage, {
+			marumie: client.secret.id,
+			seskey: client.secret.key,
+			bmark,
+			rate,
+		});
+		if (!client.secret.chatload) return;
+		const result = new Object();
+		result.client = client;
+		result.guild = client.user.guild;
+		if (obj.coments.length) {
+			bmark = obj.bmark || bmark;
+			let c = obj.coments[0].source;
+		if (!client.secret.chatload) return;
+			result.author = client.secret.options.has(1n << 2n) ? await client.users.get(obj.coments[0].uid) : await client.users.fetch(obj.coments[0].uid);
+			if (
+				c.includes(
+					"<a href='javascript:void(0);' class='astyle' onclick='PhotoGet(this,"
+				)
+			) {
+				result.type = "image";
+				let pid = c.split("PhotoGet(this,")[1];
+				let pkey = pid.split(',"')[1].split('")')[0];
+				pid = pid.split(",")[0];
+				if (!client.secret.chatload) return;
+				let tag = await api.post(api.links.Attachment.PhotoGet, {
+					marumie: client.secret.id,
+					seskey: client.secret.key,
+					imgid: pid,
+					imgpass: pkey,
+				})
+				tag = tag.source
+				if (tag.includes(".png")) {
+					tag = ".png";
+				} else if (tag.includes(".jpg")) {
+					tag = ".jpg";
+				} else if (tag.includes(".gif")) {
+					tag = ".gif";
+				} else if (tag.includes(".jpeg")) {
+					tag = ".jpeg";
+				}
+				result.content = null;
+				const { GuildMessageAttachMent } = require("../structures/FileAttachent")
+				const photoData = await api.post(api.links.Attachment.PhotoData(pid, pkey, tag),{}, 2).then(n => n.stream())
+				result.file = new GuildMessageAttachMent(client, photoData, pid)
+			} else {
+				result.type = "text";
+				result.file = null;
+				c = convtext(
+					c
+						.split("\t")
+						.join("")
+						.split("<td class='c_mozi' style='color:#000000'>")[1]
+						.split("\n")[0]
+				);
+				result.content = c
+			}
+			if (!client.secret.chatload) return;
+			client.emit("GuildMessageCreate", result);
+		}
+		if (!client.secret.chatload) return;
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+	}
+}
