59080b1acd2f2d0faeb236cd6d8ac74f655fc1e9
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-21 12:29:29 +0900

    DM受信用の更新

--- src/collectors/BaseMessageCollector.js
+++ src/collectors/BaseMessageCollector.js
@@ -1,9 +1,11 @@
 const { api, convtext } = require("../utils/");
 
 let load = false
-exports.startload = function (client, kbmark) {
-	const GuildChat = require("./GuildChatCollector")
-	const AreaChat = require("./AreaChatCollector")
-	if (client.user.guild?.id && client.secret.options.has(1n << 1n)) GuildChat(client);
-	if (client.secret.options.has(1n << 0n)) AreaChat(client, kbmark)
+exports.startload = function (client, kbmark, hbmark) {
+	const GuildMessage = require("./GuildMessageCollector")
+	const AreaMessage = require("./AreaMessageCollector")
+	const DirectMessage = require("./DirectMessageCollector")
+	if (client.user.guild?.id && client.secret.options.has(1n << 1n)) GuildMessage(client);
+	if (client.secret.options.has(1n << 0n)) AreaMessage(client, kbmark)
+	if (client.secret.options.has(1n << 4n)) DirectMessage(client, hbmark)
 };
