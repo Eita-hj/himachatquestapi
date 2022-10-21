ab9b23f4fb1cb73c4828783913d25b203261205e
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-21 09:56:49 +0900

    ファイル分け

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,2 @@
 exports.api = require("./api")
-exports.convtext = function (text) {
-	return text
-		.split("<small style='color:#AAAAAA'>")[0]
-		.split("&#039;")
-		.join("'")
-		.split("&quot;")
-		.join('"')
-		.split("&lt;")
-		.join("<")
-		.split("&gt;")
-		.join(">")
-		.split("&amp;")
-		.join("&");
-};
+exports.convtext = require("./TextConvert")
