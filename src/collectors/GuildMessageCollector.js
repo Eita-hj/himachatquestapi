e623d7265278079d8c35f7097e8358ec6b969043
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-21 13:31:25 +0900

    ファイル関連の不具合を修正

--- src/collectors/GuildMessageCollector.js
+++ src/collectors/GuildMessageCollector.js
@@ -51,7 +51,7 @@ module.export = async function (client) {
 						tag = ".jpeg";
 					}
 					result.content = null;
-					const GuildMessageAttachMent = require("../structures/GuildFileAttachent")
+					const GuildMessageAttachMent = require("../structures/GuildMessageAttachent")
 					const photoData = await api.post(api.links.Attachment.PhotoData(pid, pkey, tag),{}, 2).then(n => n.stream())
 					result.file = new GuildMessageAttachMent(client, photoData, pid)
 				} else {
