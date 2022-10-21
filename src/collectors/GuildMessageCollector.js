ddbdce64d3e354f00cf8270c4af9bffb64758163
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-10-21 13:28:46 +0900

    ファイルを受信したらエラーが出る不具合を修正

--- src/collectors/GuildMessageCollector.js
+++ src/collectors/GuildMessageCollector.js
@@ -20,50 +20,52 @@ module.export = async function (client) {
 		result.guild = client.user.guild;
 		if (obj.coments.length) {
 			bmark = obj.bmark || bmark;
-			let c = obj.coments[0].source;
-		if (!client.secret.chatload) return;
-			result.author = client.secret.options.has(1n << 2n) ? await client.users.get(obj.coments[0].uid) : await client.users.fetch(obj.coments[0].uid);
-			if (
-				c.includes(
-					"<a href='javascript:void(0);' class='astyle' onclick='PhotoGet(this,"
-				)
-			) {
-				result.type = "image";
-				let pid = c.split("PhotoGet(this,")[1];
-				let pkey = pid.split(',"')[1].split('")')[0];
-				pid = pid.split(",")[0];
+			for (let i = 0; i < obj.coments.length; i++){
+				let c = obj.coments[i].source;
 				if (!client.secret.chatload) return;
-				let tag = await api.post(api.links.Attachment.PhotoGet, {
-					marumie: client.secret.id,
-					seskey: client.secret.key,
-					imgid: pid,
-					imgpass: pkey,
-				})
-				tag = tag.source
-				if (tag.includes(".png")) {
-					tag = ".png";
-				} else if (tag.includes(".jpg")) {
-					tag = ".jpg";
-				} else if (tag.includes(".gif")) {
-					tag = ".gif";
-				} else if (tag.includes(".jpeg")) {
-					tag = ".jpeg";
+				result.author = client.secret.options.has(1n << 2n) ? await client.users.get(obj.coments[i].uid) : await client.users.fetch(obj.coments[i].uid);
+				if (
+					c.includes(
+						"<a href='javascript:void(0);' class='astyle' onclick='PhotoGet(this,"
+					)
+				) {
+					result.type = "image";
+					let pid = c.split("PhotoGet(this,")[1];
+					let pkey = pid.split(',"')[1].split('")')[0];
+					pid = pid.split(",")[0];
+					if (!client.secret.chatload) return;
+					let tag = await api.post(api.links.Attachment.PhotoGet, {
+						marumie: client.secret.id,
+						seskey: client.secret.key,
+						imgid: pid,
+						imgpass: pkey,
+					})
+					tag = tag.source
+					if (tag.includes(".png")) {
+						tag = ".png";
+					} else if (tag.includes(".jpg")) {
+						tag = ".jpg";
+					} else if (tag.includes(".gif")) {
+						tag = ".gif";
+					} else if (tag.includes(".jpeg")) {
+						tag = ".jpeg";
+					}
+					result.content = null;
+					const GuildMessageAttachMent = require("../structures/GuildFileAttachent")
+					const photoData = await api.post(api.links.Attachment.PhotoData(pid, pkey, tag),{}, 2).then(n => n.stream())
+					result.file = new GuildMessageAttachMent(client, photoData, pid)
+				} else {
+					result.type = "text";
+					result.file = null;
+					c = convtext(
+						c
+							.split("\t")
+							.join("")
+							.split("<td class='c_mozi' style='color:#000000'>")[1]
+							.split("\n")[0]
+					);
+					result.content = c
 				}
-				result.content = null;
-				const { GuildMessageAttachMent } = require("../structures/FileAttachent")
-				const photoData = await api.post(api.links.Attachment.PhotoData(pid, pkey, tag),{}, 2).then(n => n.stream())
-				result.file = new GuildMessageAttachMent(client, photoData, pid)
-			} else {
-				result.type = "text";
-				result.file = null;
-				c = convtext(
-					c
-						.split("\t")
-						.join("")
-						.split("<td class='c_mozi' style='color:#000000'>")[1]
-						.split("\n")[0]
-				);
-				result.content = c
 			}
 			if (!client.secret.chatload) return;
 			client.emit("GuildMessageCreate", result);
