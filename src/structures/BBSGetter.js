8de33ebb6e91558038b29a471014ca9e10d69566
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-02 14:51:19 +0900

    Client.BBSsにあたるBBSGetterNOO

---
+++ src/structures/BBSGetter.js
@@ -0,0 +1,27 @@
+const {api, convtext} = require("../utils/")
+const BaseManager = require("./BaseManager")
+const GuildBBS = require("../structures/BBS")
+
+module.exports = class BBSGetter extends BaseManager {
+  constructor(client){
+    super(client)
+  }
+  async fetch(id){
+    if (!typeof id === "number") throw new TypeError(`${id} is invalid.`)
+    const f = await api.post(api.links.Guild.BBS.Window, {
+      marumie: this.client.secret.id,
+      seskey: this.client.secret.key,
+      bbsid: id,
+      page: 1
+    })
+    const result = {}
+    const { source } = f
+    r.title = convtext(source.split("<h3 style='font-weight:bold' class='bbsul_title'>")[1].split("</h3>")[0])
+    r.author = await this.client.users.get(Number(source.split("author <span onclick='UserWindow(")[1].split(")")))
+    const t = source.split.split("</span>")
+    r.createdTimestamp = Date.parse(t[t.length - 1].slice(1).slice(0, -6).split("-").join("/"))
+    r.createdAt = new Date(createdTimestamp)
+    r.comments = new GuildBBSCommentManager(this)
+    return new GuildBBS(r, this.client)
+  }
+}
