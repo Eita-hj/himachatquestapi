e0b554451ba28586842c920be68676fbf9e0d841
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-02 15:07:16 +0900

    BBSCacheのオプションを追加

--- src/structures/OptionBits.js
+++ src/structures/OptionBits.js
@@ -4,9 +4,10 @@ module.exports = new Bits(
 		default: {
 			AreaMessages: 1n << 0n,
 			GuildMessages: 1n << 1n,
-			UsersCache: 1n << 2n,
+			UserCache: 1n << 2n,
 			GuildCache: 1n << 3n,
-			DirectMessages: 1n << 4n
+			DirectMessages: 1n << 4n,
+			BBSCache: 1n << 5n
 		}
 	}
 )
