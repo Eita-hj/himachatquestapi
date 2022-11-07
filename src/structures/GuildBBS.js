42381c21f12bb17e23888250d11722bd2161f1b8
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-07 16:09:43 +0900

    BBSの発言が取得できない不具合の修正

--- src/structures/GuildBBS.js
+++ src/structures/GuildBBS.js
@@ -4,6 +4,7 @@ const Data = require("./Data")
 module.exports = class GuildBBS extends Data {
   constructor(a, b, c){
     super(a, b)
-    if (c) this.delete = null
+    if (!c) this.delete = null
+    this.comments = new GuildBBSCommentManager(this.client, this)
   }
 }
