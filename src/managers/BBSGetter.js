a9d2b4993ba7707f2fe6d62b2eae1711e8c60024
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-07 13:35:24 +0900

    ファイルが見つからないエラーの対処

--- src/managers/BBSGetter.js
+++ src/managers/BBSGetter.js
@@ -1,5 +1,6 @@
 const {api, convtext} = require("../utils/")
 const BaseManager = require("./BaseManager")
+const GuildBBSCommentManager = require("./GuildBBSCommentManager")
 const GuildBBS = require("../structures/GuildBBS")
 
 module.exports = class BBSGetter extends BaseManager {
