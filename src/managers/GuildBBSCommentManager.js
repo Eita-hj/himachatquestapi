22a39941fede71480c177ded66669222da1f21d4
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-09 10:31:28 +0900

    BBSの発言データが取得できない不具合の修正

--- src/managers/GuildBBSCommentManager.js
+++ src/managers/GuildBBSCommentManager.js
@@ -17,6 +17,7 @@ module.exports = class GuildBBSCommentManager extends BaseManager {
     const { source } = f
     const a = source.split("<small onclick='UserWindow(")
     a.shift()
+    const r = []
     for (let i = 0; i < a.length; i++){
       const n = a[i]
       const authorid = n.split(")")[0]
@@ -24,8 +25,8 @@ module.exports = class GuildBBSCommentManager extends BaseManager {
       const number = Number(n.split(">")[1].split("　")[0])
       const createdTimestamp = Date.parse(n.split("<span style='color:#AAAAAA'>")[1].split("</span>")[0].split("-").join("/"))
       const createdAt = new Date(createdTimestamp)
-      const content = convtext(n.split("<p class='bbsul'>\r\n")[1].split("</p>")[0].split("<br />\n").join("\n"))
-      return [number, new GuildBBSCommentData({number, content, author, createdAt, createdTimestamp}, this.client)]
+      const content = convtext(n.split("<p class='bbsul_honbun'>")[1].split("</p>")[0].split("<br />\n").join("\n"))
+      r.push([number, new GuildBBSCommentData({number, content, author, createdAt, createdTimestamp}, this.client)])
     }
     return new Cache(r)
   }
