5cedd5724b6b7b61a76bf2b73a0503c029c47d89
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-09 10:18:38 +0900

    BBSの発言取得時にエラーが発生する不具合の修正

--- src/managers/GuildBBSCommentManager.js
+++ src/managers/GuildBBSCommentManager.js
@@ -17,15 +17,16 @@ module.exports = class GuildBBSCommentManager extends BaseManager {
     const { source } = f
     const a = source.split("<small onclick='UserWindow(")
     a.shift()
-    const r = a.map(async n => {
+    for (let i = 0; i < a.length; i++){
+      const n = a[i]
       const authorid = n.split(")")[0]
       const author = await this.client.users.fetch(Number(authorid))
       const number = Number(n.split(">")[1].split("　")[0])
-      const createdTimestamp = Data.parse(n.split("<span style='color:#AAAAAA'>")[1].split("</span>")[0].split("-").join("/"))
+      const createdTimestamp = Date.parse(n.split("<span style='color:#AAAAAA'>")[1].split("</span>")[0].split("-").join("/"))
       const createdAt = new Date(createdTimestamp)
       const content = convtext(n.split("<p class='bbsul'>\r\n")[1].split("</p>")[0].split("<br />\n").join("\n"))
       return [number, new GuildBBSCommentData({number, content, author, createdAt, createdTimestamp}, this.client)]
-    })
+    }
     return new Cache(r)
   }
 }
