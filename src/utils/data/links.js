6924174c172f3f626a2a72a40c65b0365789a131
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-10 11:15:00 +0900

    更新に伴うリンクを追加

--- src/utils/data/links.js
+++ src/utils/data/links.js
@@ -8,14 +8,19 @@ module.exports = {
 		BBS: {
 			Window: "http://himaquest.com/guild_BBSWindow.php",
 			List: "http://himaquest.com/guild_BBSSerch.php",
-			Create: ""
+			Create: "http://himaquest.com/guild_BBSCreate.php",
+			Send: "http://himaquest.com/guild_BBSToukou.php"
 		}
 	},
 	User: {
 		Info: "http://himaquest.com/UserWindow.php",
 		JoinGuilds: "http://himaquest.com/load_MyGuildList.php",
 		Manage: "http://himaquest.com/block_UserKanriWindow.php",
-		SendDM: "http://himaquest.com/chat_HatugenKobetu.php"
+		SendDM: "http://himaquest.com/chat_HatugenKobetu.php",
+		Setting: {
+			NameChange: "http://himaquest.com/myhouse_NameChange.php",
+			ProfileChange: "http://himaquest.com/user_ShoukaiEdit.php"
+		}
 	},
 	Chat: {
 		AreaMessage: "http://himaquest.com/chat_F5User.php",
@@ -26,8 +31,9 @@ module.exports = {
 	UpdateInfo: "http://himaquest.com/top_UpdateInfo.php",
 	Attachment: {
 		Upload:{ 
-			Guild: "http://himaquest.com/photo_UploadGuild.php/",
-			DM: "http://himaquest.com/photo_UploadKobetu.php/"
+			Guild: "http://himaquest.com/photo_UploadGuild.php",
+			DM: "http://himaquest.com/photo_UploadKobetu.php",
+			BBS: "http://himaquest.com/photo_UploadBBS.php"
 		},
 		Delete: "http://himaquest.com/photo_Delete.php",
 		PhotoGet: "http://himaquest.com/photo_Get.php",
