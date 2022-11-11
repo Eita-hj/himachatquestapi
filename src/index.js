16bd03dd2a7cd5970a15671b2294bb034861e836
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-11 12:32:54 +0900

    .GenerateToken(data)を追加

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ module.exports.Client = require("./client/");
 module.exports.OptionBits = require("./structures/OptionBits");
 module.exports.Errors = require("./structures/Errors");
 module.exports.Game = require("./structures/Game");
+module.exports.GenerateToken = require("./utils/GenerateToken").toToken
