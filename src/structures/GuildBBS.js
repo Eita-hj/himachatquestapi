619a79a5e52a7487164d870b912521ea02d2da39
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-14 13:27:56 +0900

    .send()を追加(まだ未完成)

--- src/structures/GuildBBS.js
+++ src/structures/GuildBBS.js
@@ -3,9 +3,21 @@ const Data = require("./Data")
 const GuildBBSCommentManager = require("../managers/GuildBBSCommentManager")
 
 module.exports = class GuildBBS extends Data {
-  constructor(a, b, c){
-    super(a, b)
-    if (!c) this.delete = null
-    this.comments = new GuildBBSCommentManager(this.client, this)
-  }
+	constructor(a, b, c){
+		super(a, b)
+		if (!c) this.delete = null
+		this.comments = new GuildBBSCommentManager(this.client, this)
+	}
+	async send(data){
+		if (!data) throw new TypeError("Cannot send empty message")
+		if (typeof data === "string"){
+			await api.post(api.links.Guild.BBS.Send, {
+				marumie: this.client.secret.id,
+				seskey: this.client.secret.key,
+				fnaiyou: data,
+				imgarray: [],
+				bbsid: this.id
+			})
+		}
+	}
 }
