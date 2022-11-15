e8ef769517f2dbc8a41054e702663f2c7509cb34
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-15 12:08:59 +0900

    .LoginTypeを追加

--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ module.exports.OptionBits = require("./structures/OptionBits");
 module.exports.Errors = require("./structures/Errors");
 module.exports.Game = require("./structures/Game");
 module.exports.GenerateToken = require("./utils/GenerateToken").toToken
+module.exports.LoginType = require("./structures/LoginType")
