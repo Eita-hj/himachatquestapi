1c319adfce57bb7b7e6a00cd7fb02b9872f36df5
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-11-18 23:23:12 +0900

    チャットが読み込まれない不具合の修正

--- src/collectors/BaseMessageCollector.js
+++ src/collectors/BaseMessageCollector.js
@@ -5,6 +5,7 @@ exports.startload = function (client, kbmark, hbmark) {
 	const GuildMessage = require("./GuildMessageCollector")
 	const AreaMessage = require("./AreaMessageCollector")
 	const DirectMessage = require("./DirectMessageCollector")
+	if (client.secret.options.has(1n << 0n) || (client.user.guild?.id && client.secret.options.has(1n << 1n)) || client.secret.options.has(1n << 4n)) client.secret.chatload = true
 	if (client.user.guild?.id && client.secret.options.has(1n << 1n)) GuildMessage(client);
 	if (client.secret.options.has(1n << 0n)) AreaMessage(client, kbmark)
 	if (client.secret.options.has(1n << 4n)) DirectMessage(client, hbmark)
