b74632be33fa6854bc99a0d6efe1236aadcadb62
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-13 14:49:29 +0900

    OptionBitsの名称変更に伴う修正

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 module.exports.GuildMessageAttachment = require("./structures/GuildMessageAttachment");
 module.exports.DirectMessageAttachment = require("./structures/DirectMessageAttachment");
 module.exports.Client = require("./client/");
-module.exports.OptionBits = require("./structures/OptionBits");
+module.exports.ClientOptionBits = require("./structures/ClientOptionBits");
 module.exports.Errors = require("./structures/Errors");
 module.exports.Game = require("./structures/Game");
 module.exports.GenerateToken = require("./utils/GenerateToken").toToken
