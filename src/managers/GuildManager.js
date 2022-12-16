54eb250048a3814301a851ed612da4c844826fc0
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-16 12:33:19 +0900

    .fetchと.getの配列対応

--- src/managers/GuildManager.js
+++ src/managers/GuildManager.js
@@ -3,39 +3,87 @@ const { api, convtext } = require("../utils/")
 const Guild = require("../structures/Guild")
 
 module.exports = class GuildManager extends BaseManager {
-	async fetch(id) {
-		if (isNaN(id)) throw new Error(`${id} is invalid. (Error Code 500)`)
-		if (!(typeof Number(id) === "number" && Number.isInteger(Number(id)) && id > 0)) throw new Error(`${id} is invalid. (Error Code 501)`)
-		let source = await api.post(api.links.Guild.Info, {
-			marumie: this.client.secret.id,
-			seskey: this.client.secret.key,
-			targetid: id,
-		});
-		source = source.source;
-		const result = new Object();
-		result.id = id;
-		result.name = convtext(
-			source
-				.split("<div class='gw_guildname'>")[1]
-				.split("</div>")[0]
-		);
-	if (result.name == "") return null
-		result.PR = convtext(source.split("<div class='gw_guildpr'>")[1].split("</div>")[0]);
-		result.info = convtext(
-			source
-				.split("<div class='gw_setumei'>")[1]
-				.split("</div>")[0]
-				.split("<br />")
-				.join("\n")
-		);
-		result.ownerID = source
-			.split("<span onclick='UserWindow(")[1]
-			.split(")")[0];
-		result.owner = result.ownerID == this.client.secret.id ? this.user : await this.client.users.get(result.ownerID)
-		if (this.client.secret.options.has(1n << 2n)) {
-			if (this.cache.has(result.id)) this.cache.delete(result.id);
-			this.cache.set(result.id, result);
+	async fetch(data) {
+		switch (typeof data){
+			case "number":
+			case "string":
+				if (isNaN(data)) throw new Error(`${data} is invalid. (Error Code 500)`)
+				if (!(typeof Number(data) === "number" && Number.isInteger(Number(data)) && Number(data) > 0)) throw new Error(`${data} is invalid. (Error Code 501)`)
+				let source = await api.post(api.links.Guild.Info, {
+					marumie: this.client.secret.id,
+					seskey: this.client.secret.key,
+					targetid: data,
+				});
+				source = source.source;
+				const result = new Object();
+				result.id = data;
+				result.name = convtext(
+					source
+						.split("<div class='gw_guildname'>")[1]
+						.split("</div>")[0]
+				);
+				if (result.name == "") return null
+				result.PR = convtext(source.split("<div class='gw_guildpr'>")[1].split("</div>")[0]);
+				result.info = convtext(
+					source
+						.split("<div class='gw_setumei'>")[1]
+						.split("</div>")[0]
+						.split("<br />")
+						.join("\n")
+				);
+				result.ownerID = source
+					.split("<span onclick='UserWindow(")[1]
+					.split(")")[0];
+				result.owner = result.ownerID == this.client.secret.id ? this.user : await this.client.users.get(result.ownerID)
+				if (this.client.secret.options.has(1n << 2n)) {
+					if (this.cache.has(result.id)) this.cache.delete(result.id);
+					this.cache.set(result.id, result);
+				}
+				return new Guild(result, this.client);
+			case "object":
+				const cache = new Cache()
+				if (Array.isArray(data)){
+					for (let i = 0; i < data.length; i++){
+						const id = data[i]
+						if (typeof id === "number" || typeof id === "string"){
+							const guild = await this.fetch(id)
+							cache.set(string(id), guild)
+						} else {
+							throw new TypeError(`${id} must be string, or number.`)
+						}
+					}
+					return cache;
+				} else {
+					throw new TypeError(`${data} must be array<string | number>, string, or number.`)
+				}
+			default:
+				throw new TypeError(`${data} must be array<string | number>, string, or number.`)
+		}	
+	}
+	async get(data){
+		switch (typeof data){
+			case "number":
+			case "string":
+				return this.cache.has(data) ? this.cache.get(data) : await this.fetch(data);
+				break;
+			case "object":
+				const cache = new Cache()
+				if (Array.isArray(data)){
+					for (let i = 0; i < data.length; i++){
+						const id = data[i]
+						if (typeof id === "number" || typeof id === "string"){
+							const guild = await this.get(id)
+							cache.set(string(id), guild)
+						} else {
+							throw new TypeError(`${id} must be string, or number.`)
+						}
+					}
+					return cache;
+				} else {
+					throw new TypeError(`${data} must be array<string | number>, string, or number.`)
+				}
+			default:
+				throw new TypeError(`${data} must be array<string | number>, string, or number.`)
 		}
-		return new Guild(result, this.client);
 	}
 }
