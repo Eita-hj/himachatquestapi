cbb6e1573e9c72148a74d9aa7368c15a48ec0046
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-20 13:48:08 +0900

    GameからGameClientに変更

---
+++ src/client/GameClient.js
@@ -0,0 +1,43 @@
+const { api, convtext } = require("../utils/");
+const Cache = require("../structures/Cache");
+const BaseClient = require("./BaseClient")
+const RankingManager = require("../managers/RankingManager")
+
+module.exports = class GameClient extends BaseClient {
+	constructor(client) {
+		this.ranking = new RankingManager(client);
+	}
+	async getUpdateInfo(date = null) {
+		let result = new Cache(),
+			temp = new Array();
+		const { source } = await api.post(api.links.UpdateInfo);
+		const results = source
+			.split("<BR>")
+			.join("<br>")
+			.slice(33)
+			.slice(0, -12)
+			.split("<br>\r\n");
+		for (let i = results.length - 1; i >= 0; i--) {
+			if (
+				/^[0-9]{4}\/([1-9]|1[0-2])\/([1-9]|[12][0-9]|3[01])/.test(
+					results[i].trim()
+				)
+			) {
+				result.set(results[i], temp);
+				temp = new Array();
+			} else {
+				temp.unshift(results[i]);
+			}
+			if (date) {
+				if (typeof date == "string") {
+					return result.get(date);
+				} else {
+					return result.get(
+						`${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+					);
+				}
+			}
+			return result;
+		}
+	}
+};
