470bb54fa9b044f884cf7784c018dce1c9fafb35
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-20 13:59:28 +0900

    無視リストの管理を行うClientUserIgnoreManagerの追加

---
+++ src/managers/ClientUserIgnoreManager.js
@@ -0,0 +1,55 @@
+const BaseManager = require("./BaseManager")
+const User = require("../structures/User")
+const Cache = require("../structures/Cache")
+const { api, convtext } = require("../utils/")
+
+class ClientUserIgnoreManager extends BaseManager {
+	async fetch() {
+		const { musiarray } = await api.post(api.links.User.Ignores.List,{
+			marumie: this.client.secret.id,
+			seskey: this.client.secret.key,
+		})
+		const result = new Cache()
+		for (let i = 0; i < musiarray.length; i++){
+			const u = await this.client.users.get(musiarray[i])
+			result.set(musiarray[i], u)
+		}
+		this.cache = result
+		return result
+	}
+	async add(target) {
+		if (target instanceof User){
+			await api.post(api.links.User.Ignores.Add,{
+				marumie: this.client.secret.id,
+				seskey: this.client.secret.key,
+				tuid: target.id,
+				tmemo: target.name
+			})
+		} else {
+			if (!Number(Number(target)).isSafeInteger()) throw new TypeError(`${target} is invalid.`)
+			await api.post(api.links.User.Ignores.Add,{
+				marumie: this.client.secret.id,
+				seskey: this.client.secret.key,
+				tuid: target
+			})
+		}
+	}
+	async remove(target) {
+		if (target instanceof User){
+			await api.post(api.links.User.Ignores.Remove,{
+				marumie: this.client.secret.id,
+				seskey: this.client.secret.key,
+				targetid: target.id
+			})
+		} else {
+			if (!Number(Number(target)).isSafeInteger()) throw new TypeError(`${target} is invalid.`)
+			const r = await api.post(api.links.User.Ignores.Remove,{
+				marumie: this.client.secret.id,
+				seskey: this.client.secret.key,
+				targetid: target
+			})
+		}
+	}
+}
+
+module.exports = ClientUserIgnoreManager
