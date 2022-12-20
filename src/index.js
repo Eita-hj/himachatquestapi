7f0f5b464d7ef1641bdd1abaa7a317ba2ecc17e8
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-20 13:33:40 +0900

    GameClientの追加に伴う変更

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-"use strict";
-
 module.exports.GuildMessageAttachment = require("./structures/GuildMessageAttachment");
 module.exports.DirectMessageAttachment = require("./structures/DirectMessageAttachment");
-module.exports.Client = require("./client/");
+module.exports.Client = require("./client/").Client;
+module.exports.GameClient = require("./client/").GameClient;
 module.exports.ClientOptionBits = require("./structures/ClientOptionBits");
 module.exports.Errors = require("./structures/Errors");
 module.exports.Game = require("./structures/Game");
-module.exports.GenerateToken = require("./utils/GenerateToken").toToken
-module.exports.LoginType = require("./structures/LoginType")
+module.exports.GenerateToken = require("./utils/GenerateToken").toToken;
+module.exports.LoginType = require("./structures/LoginType");
