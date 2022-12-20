d008bf45d401ba025cd820d1991c8b0c985eeb1c
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-20 13:51:59 +0900

    ClientOptionの仕様変更に伴う更新

--- src/structures/ClientOptionBits.js
+++ src/structures/ClientOptionBits.js
@@ -2,12 +2,10 @@ const Bits = require("./Bits")
 module.exports = new Bits(
 	{
 		default: {
-			AreaMessages: 1n << 0n,
-			GuildMessages: 1n << 1n,
-			UserCache: 1n << 2n,
-			GuildCache: 1n << 3n,
-			DirectMessages: 1n << 4n,
-			BBSCache: 1n << 5n
+			DirectMessages: 1n << 0n,
+			AreaMessages: 1n << 1n,
+			GuildMessages: 1n << 2n,
+			GuildBBSs: 1n << 3n,
 		}
 	}
 )
