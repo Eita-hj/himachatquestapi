65c4d4de537472c36e0da449dc69b1628a2dde54
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-21 10:59:18 +0900

    GuildBBSComments Cache Optionの追加

--- src/structures/ClientCacheOptionBits.js
+++ src/structures/ClientCacheOptionBits.js
@@ -4,8 +4,9 @@ module.exports = new Bits({
 		Users: 1n << 0n,
 		Guilds: 1n << 1n,
 		GuildBBSs: 1n << 2n,
-		ClientUserFriends: 1n << 3n,
-		ClientUserIgnoreList: 1n << 4n,
-		GameRanking: 1n << 5n
+		GuildBBSComments: 1n << 3n,
+		ClientUserFriends: 1n << 4n,
+		ClientUserIgnoreList: 1n << 5n,
+		GameRanking: 1n << 6n
 	}
 })
