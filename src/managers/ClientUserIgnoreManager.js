e5a13e060baa91b1f95e9962e3e41b69f375aebe
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-25 19:47:50 +0900

    無視リス登録/解除時に発生するエラーの対処

--- src/managers/ClientUserIgnoreManager.js
+++ src/managers/ClientUserIgnoreManager.js
@@ -26,7 +26,7 @@ class ClientUserIgnoreManager extends BaseManager {
 				tmemo: target.name
 			})
 		} else {
-			if (!Number(Number(target)).isSafeInteger()) throw new TypeError(`${target} is invalid.`)
+			if (!Number.isSafeInteger(Number(target))) throw new TypeError(`${target} is invalid.`)
 			await api.post(api.links.User.Ignores.Add,{
 				marumie: this.client.secret.id,
 				seskey: this.client.secret.key,
@@ -44,7 +44,7 @@ class ClientUserIgnoreManager extends BaseManager {
 				targetid: target.id
 			})
 		} else {
-			if (!Number(Number(target)).isSafeInteger()) throw new TypeError(`${target} is invalid.`)
+			if (!Number.isSafeInteger(Number(target))) throw new TypeError(`${target} is invalid.`)
 			const r = await api.post(api.links.User.Ignores.Remove,{
 				marumie: this.client.secret.id,
 				seskey: this.client.secret.key,
