44c674eccc24dbe7f7c30f1e284e64463fb1bae3
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2022-12-30 17:29:49 +0900

    fetch時にundefinedを返す不具合を修正

--- src/managers/ClientUserFriendsManager.js
+++ src/managers/ClientUserFriendsManager.js
@@ -5,45 +5,46 @@ const ClientUserFriendRequestsManager = require("./ClientUserFriendRequestsManag
 const FriendUser = require("../structures/FriendUser");
 
 class ClientUserFriendsManager extends BaseManager {
-  constructor(client) {
-    super(client)
-		if (!client.secret.caches.has(1n << 4n)) delete this.cache
-    this.recieves = new ClientUserFriendRequestsManager(client);
-  }
-  async fetch() {
-    const { frienddata } = await api.post(api.links.User.Friends.List, {
-      marumie: this.client.secret.id,
-      seskey: this.client.secret.key,
-    });
-    const users = frienddata.map((user) => ({
-      id: user.friend,
-      lastlogined: user.updated,
-    }));
-    const data = new Cache();
-    for (let i = 0; i < users.length; i++) {
-      const userdata = users[i];
-      const user = await this.client.users.get(userdata.id);
-      const lastLoginedTimestamp = Date.parse(
-        userdata.lastlogined.split("-").join("/")
-      );
-      const lastLoginedAt = new Date(lastLoginedTimestamp);
-      user.save({
-        lastLoginedTimestamp,
-        lastLoginedAt,
-      });
-      const frienduser = new FriendUser(user.getData());
-      if (this.client.secret.caches.has(1n << 4n)) data.set(user.id, frienduser);
-    }
-    return data;
-  }
-  async delete(data) {
-    const userId = data instanceof FriendUser ? data.id : data;
-    if (!userId || !Number.isSafeInteger(Number(userId)))
-      throw new TypeError("FriendDeleteData must be FriendUser or userid.");
-    await api.post(api.links.User.Friends.Delete, {
-      marumie: this.client.secret.id,
-      seskey: this.client.secret.key,
-      sinseiid: userId,
-    });
-  }
+	constructor(client) {
+	super(client)
+		if (!client.secret.caches.has(1n << 4n)) delete this.cache;
+		this.recieves = new ClientUserFriendRequestsManager(client);
+	};
+	async fetch() {
+		const { frienddata } = await api.post(api.links.User.Friends.List, {
+			marumie: this.client.secret.id,
+			seskey: this.client.secret.key,
+		});
+		const users = frienddata.map((user) => ({
+			id: user.friend,
+			lastlogined: user.updated,
+		}));
+		const data = new Cache();
+		for (let i = 0; i < users.length; i++) {
+			const userdata = users[i];
+			const user = await this.client.users.get(userdata.id);
+			const lastLoginedTimestamp = Date.parse(
+				userdata.lastlogined.split("-").join("/")
+			);
+			const lastLoginedAt = new Date(lastLoginedTimestamp);
+				user.save({
+				lastLoginedTimestamp,
+				lastLoginedAt,
+			});
+			const frienduser = new FriendUser(user.getData());
+			data.set(user.id, frienduser);
+			if (this.client.secret.caches.has(1n << 4n)) this.cache.set(user.id, frienduser);
+		}
+		return data;
+	}
+	async delete(data) {
+		const userId = data instanceof FriendUser ? data.id : data;
+		if (!userId || !Number.isSafeInteger(Number(userId)))
+			throw new TypeError("FriendDeleteData must be FriendUser or userid.");
+		await api.post(api.links.User.Friends.Delete, {
+			marumie: this.client.secret.id,
+			seskey: this.client.secret.key,
+			sinseiid: userId,
+		});
+	}
 }
