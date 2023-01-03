4efa1464412352b88149e7d8c1c04d0da7c45ebe
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-01-03 14:47:45 +0900

    フレンド解除ができない不具合を修正

--- src/managers/ClientUserFriendsManager.js
+++ src/managers/ClientUserFriendsManager.js
@@ -44,7 +44,7 @@ module.exports = class ClientUserFriendsManager extends BaseManager {
 		await api.post(api.links.User.Friends.Delete, {
 			marumie: this.client.secret.id,
 			seskey: this.client.secret.key,
-			sinseiid: userId,
+			tuid: userId,
 		});
 		if (this.client.secret.caches.has(1n << 4n)){
 			const user = await this.client.users.get(userId)
