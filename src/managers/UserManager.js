899c025d647e14a28e503caa9c5813ca2e13f32f
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-01-11 20:24:31 +0900

    ギルドが未所属のときの挙動を変更+デバッグ部分削除

--- src/managers/UserManager.js
+++ src/managers/UserManager.js
@@ -61,8 +61,7 @@ module.exports = class UserManager extends BaseManager {
 					result.guild = {
 						name: convtext(source.source.split("<span style='color:#AAAAAA;font-size:8px;vertical-align:super;float:left;'>所属</span>\r\n")[1].split("</div>")[0].split(/\s/).join(""))
 					}
-					console.log(result.guild.name)
-					if (result.guild.name == "<span style='color:#AAAAAA;font-size:14px;'>(未設定)</span>") result.guild = undefined
+					if (result.guild.name == "<spanstyle='color:#AAAAAA;font-size:14px;'>(未設定)</span>") result.guild = undefined
 					if (data == this.client.secret.id) {
 						let guildid = await api.post(api.links.User.JoinGuilds, {
 							marumie: this.client.secret.id,
