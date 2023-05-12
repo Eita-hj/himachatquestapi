14618e9777479fcfd45c776a1b5cb9e37f657f7b
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-05-13 04:27:41 +0900

    スレッドの投稿削除機能を追加

--- src/structures/GuildBBSCommentData.js
+++ src/structures/GuildBBSCommentData.js
@@ -4,4 +4,12 @@ module.exports = class GuildBBSCommentData extends Data {
   constructor(a, b){
     super(a, b)
   }
+  delete() {
+    api.post(api.links.Guild.BBS.CommentDelete, {
+      origin: "himaque",
+      myid: this.client.secret.id,
+      seskey: this.client.secret.key,
+      bbstxtid: this.commentid
+    })
+  }
 }
