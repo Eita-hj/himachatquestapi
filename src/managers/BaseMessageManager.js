dd7e505ac72a7dea031e5a0f0aee23281aa7c8ef
Eita-hj <102592819+Eita-hj@users.noreply.github.com>
2023-05-19 13:50:52 +0900

    .resolve()と.resolveId()を追加

--- src/managers/BaseMessageManager.js
+++ src/managers/BaseMessageManager.js
@@ -1,8 +1,19 @@
 const BaseManager = require("./BaseManager")
+const BaseMessage = require("./structures/BaseMessage")
 module.exports = class BaseMessageManager extends BaseManager {
 	constructor(client) {
-		
+		super(client)
+		if (client.secrets.caches.has(1n << 7n)) delete this.cache
 	}
 	resolve(data) {
+		if (typeof data === "string" || typeof data === "number") {
+			return this.cache.get(data)
+		} else if (data instanceof BaseMessage) {
+			return this.resolve(data?.id)
+		}
+		return undefined
+	}
+	resolveId(data) {
+		return this.resolve(data)?.id
 	}
 }
